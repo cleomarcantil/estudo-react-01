f0212b505b0d90070622b299c613b0adfaaf1444
Cleomar Cantil <cleomar@Cleomar-Note.localdomain>
2020-06-27 23:10:33 -0300

    Arquivos iniciais do projeto.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Teste React</h1>
+      </header>
+
+      <div>
+        <p>
+          Uma aplicação simples em React
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
